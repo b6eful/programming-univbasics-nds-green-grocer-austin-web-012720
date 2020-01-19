7c7bf424258356a84cf76df1dc3aa929796af9c6
Phoenix R <phoenixrabang@gmail.com>
2020-01-19 19:43:52 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -13,11 +13,16 @@ def consolidate_cart(cart)
   carts = []
   while n < cart.length do
     cartitem = find_item_by_name_in_collection(cart[i][:item], new_cart)
-    if new_cart_item
-      new_cart_item[:count] += 1
+    if cartitem
+      cartitem[:count] += 1
     else
-      new_cart_item = {
-        :item 
+      cartitem = {
+        :item => cart[i][:item],
+        :price => cart[i][:price],
+        :clearance => cart[i][:clearance],
+        :count => 1 
+      }
+      carts << cartitem
 end
 
 def apply_coupons(cart, coupons)
